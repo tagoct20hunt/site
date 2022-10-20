f1aefb2fd41234970c2784ccf77c3b6fe066931a
tagoct20hunt <85247685+tagoct20hunt@users.noreply.github.com>
2022-10-20 16:46:46 -0700

    fuck 2

--- 2022/scores.js
+++ 2022/scores.js
@@ -44,14 +44,3 @@ function pushTable(scores, id) {
   }
   document.getElementById(id).appendChild(wrap);
 };
-
-var x = [
-  [1,1,1],
-  [2,1,2],
-  [1,3,0],
-  [5,7,1],
-  [1,4,9],
-  ["asdf","asdf","asdf"]
-];
-
-pushTable(x, 'scoreboardContainer');
